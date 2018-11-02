cb539de8d75d7aee44c3abcf1c857ed19dc252cd
Brita Ramsay <britaramsay@gmail.com>
2018-11-02 16:48:09 -0400

    moved App component contents to new file

--- src/App.js
+++ src/App.js
@@ -1,91 +1,93 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ChooseIndexButtons from './components/chooseParams/chooseIndexButtons';
-import ChooseAciParams from './components/chooseParams/chooseAciParams';
-import ChooseNdsiParams from './components/chooseParams/chooseNdsiParams';
-import ChooseAdiParams from './components/chooseParams/chooseAdiParams';
-import ChooseBioacousticParams from './components/chooseParams/chooseBioacousticParams';
-import ChooseEvennessParams from './components/chooseParams/chooseEvennessParams';
-import ChooseRmsParams from './components/chooseParams/chooseRmsParams';
+import NewJobs from './components/newJobs';
+// import ChooseIndexButtons from './components/chooseParams/chooseIndexButtons';
+// import ChooseAciParams from './components/chooseParams/chooseAciParams';
+// import ChooseNdsiParams from './components/chooseParams/chooseNdsiParams';
+// import ChooseAdiParams from './components/chooseParams/chooseAdiParams';
+// import ChooseBioacousticParams from './components/chooseParams/chooseBioacousticParams';
+// import ChooseEvennessParams from './components/chooseParams/chooseEvennessParams';
+// import ChooseRmsParams from './components/chooseParams/chooseRmsParams';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.onClickIndex = this.onClickIndex.bind(this);
-    this.handleParamChange = this.handleParamChange.bind(this);
-    this.handleJobSubmit = this.handleJobSubmit.bind(this);
-    this.cancelJob = this.cancelJob.bind(this)
+  //   this.onClickIndex = this.onClickIndex.bind(this);
+  //   this.handleParamChange = this.handleParamChange.bind(this);
+  //   this.handleJobSubmit = this.handleJobSubmit.bind(this);
+  //   this.cancelJob = this.cancelJob.bind(this)
 
-    this.state = {
-        selectedIndex: '',
-        changeIndexWarning: false,
-        params: ''
-    };
-  }
+  //   this.state = {
+  //       selectedIndex: '',
+  //       changeIndexWarning: false,
+  //       params: ''
+  //   };
+  // }
 
-  onClickIndex = (e) => {
-    // If another index is already select and the button clicked is disabled
-    // tell user to start or cancel current job
-    if(e.target.parentElement.className.indexOf('disabled') !== -1) {
-      this.setState({changeIndexWarning: true})
+  // onClickIndex = (e) => {
+  //   // If another index is already select and the button clicked is disabled
+  //   // tell user to start or cancel current job
+  //   if(e.target.parentElement.className.indexOf('disabled') !== -1) {
+  //     this.setState({changeIndexWarning: true})
 
-      setTimeout(() => {this.setState({changeIndexWarning: false})}, 3000);
-    }
-    // Otherwise set state selectedIndex to name of index
-    else {
-      this.setState({selectedIndex: e.target.id})
+  //     setTimeout(() => {this.setState({changeIndexWarning: false})}, 3000);
+  //   }
+  //   // Otherwise set state selectedIndex to name of index
+  //   else {
+  //     this.setState({selectedIndex: e.target.id})
 
-      switch(e.target.id) {
-        case 'aci': {
-          this.setState({paramComp: <ChooseAciParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
-          break;
-        }
-        case 'ndsi': {
-          this.setState({paramComp: <ChooseNdsiParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
-          break;
-        }
-        case 'adi': {
-          this.setState({paramComp: <ChooseAdiParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
-          break;
-        }
-        case 'bioacoustic': {
-          this.setState({paramComp: <ChooseBioacousticParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
-          break;
-        }
-        case 'evenness': {
-          this.setState({paramComp: <ChooseEvennessParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
-          break;
-        }
-        case 'rms': {
-          this.setState({paramComp: <ChooseRmsParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
-          break;
-        }
-      }
-    }
-  }
+  //     switch(e.target.id) {
+  //       case 'aci': {
+  //         this.setState({paramComp: <ChooseAciParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
+  //         break;
+  //       }
+  //       case 'ndsi': {
+  //         this.setState({paramComp: <ChooseNdsiParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
+  //         break;
+  //       }
+  //       case 'adi': {
+  //         this.setState({paramComp: <ChooseAdiParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
+  //         break;
+  //       }
+  //       case 'bioacoustic': {
+  //         this.setState({paramComp: <ChooseBioacousticParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
+  //         break;
+  //       }
+  //       case 'evenness': {
+  //         this.setState({paramComp: <ChooseEvennessParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
+  //         break;
+  //       }
+  //       case 'rms': {
+  //         this.setState({paramComp: <ChooseRmsParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
+  //         break;
+  //       }
+  //     }
+  //   }
+  // }
 
-  handleParamChange (e) {
-    let params = Object.assign({}, this.state.params);
+  // handleParamChange (e) {
+  //   let params = Object.assign({}, this.state.params);
 
-    params[e.target.id] = e.target.value
+  //   params[e.target.id] = e.target.value
 
-    this.setState({params: params})
-  }
+  //   this.setState({params: params})
+  // }
 
-  handleJobSubmit () {
+  // handleJobSubmit () {
 
-  }
+  // }
 
-  cancelJob () {
-    this.setState({selectedIndex: '', params: ''})
-  }
+  // cancelJob () {
+  //   this.setState({selectedIndex: '', params: ''})
+  // }
 
   render() {
     return (
       <div className="App">
-        <div className="container-fluid">
+      <NewJobs />
+        {/* <div className="container-fluid">
           <div className="row">
             <div className='col-8'>
               <h2>Start New Jobs</h2>
@@ -112,7 +114,7 @@ class App extends Component {
           <div className="row">
             <button className="btn btn-lg btn-info" onClick={this.cancelJob}>Cancel Job</button>
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }
---
+++ src/components/newJobs.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+// import {
+//     ChooseIndexButtons,
+//     ChooseAciParams,
+//     ChooseNdsiParams,
+//     ChooseAdiParams,
+//     ChooseBioacousticParams,
+//     ChooseEvennessParams,
+//     ChooseRmsParams
+// } from './chooseParams';
+import ChooseIndexButtons from './chooseParams/chooseIndexButtons';
+import ChooseAciParams from './chooseParams/chooseAciParams';
+import ChooseNdsiParams from './chooseParams/chooseNdsiParams';
+import ChooseAdiParams from './chooseParams/chooseAdiParams';
+import ChooseBioacousticParams from './chooseParams/chooseBioacousticParams';
+import ChooseEvennessParams from './chooseParams/chooseEvennessParams';
+import ChooseRmsParams from './chooseParams/chooseRmsParams';
+
+class NewJobs extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onClickIndex = this.onClickIndex.bind(this);
+    this.handleParamChange = this.handleParamChange.bind(this);
+    this.handleJobSubmit = this.handleJobSubmit.bind(this);
+    this.cancelJob = this.cancelJob.bind(this)
+
+    this.state = {
+        selectedIndex: '',
+        changeIndexWarning: false,
+        params: ''
+    };
+  }
+
+  onClickIndex = (e) => {
+    // If another index is already select and the button clicked is disabled
+    // tell user to start or cancel current job
+    if(e.target.parentElement.className.indexOf('disabled') !== -1) {
+      this.setState({changeIndexWarning: true})
+
+      setTimeout(() => {this.setState({changeIndexWarning: false})}, 3000);
+    }
+    // Otherwise set state selectedIndex to name of index
+    else {
+      this.setState({selectedIndex: e.target.id})
+
+      switch(e.target.id) {
+        case 'aci': {
+          this.setState({paramComp: <ChooseAciParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
+          break;
+        }
+        case 'ndsi': {
+          this.setState({paramComp: <ChooseNdsiParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
+          break;
+        }
+        case 'adi': {
+          this.setState({paramComp: <ChooseAdiParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
+          break;
+        }
+        case 'bioacoustic': {
+          this.setState({paramComp: <ChooseBioacousticParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
+          break;
+        }
+        case 'evenness': {
+          this.setState({paramComp: <ChooseEvennessParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
+          break;
+        }
+        case 'rms': {
+          this.setState({paramComp: <ChooseRmsParams params = {this.state.params} onChange={this.handleParamChange} onSubmit={this.handleJobSubmit} />})
+          break;
+        }
+      }
+    }
+  }
+
+  handleParamChange (e) {
+    let params = Object.assign({}, this.state.params);
+
+    params[e.target.id] = e.target.value
+
+    this.setState({params: params})
+  }
+
+  handleJobSubmit () {
+
+  }
+
+  cancelJob () {
+    this.setState({selectedIndex: '', params: ''})
+  }
+
+  render() {
+    return (
+    //   <div className="App">
+        <div className="container-fluid">
+          <div className="row">
+            <div className='col-8'>
+              <h2>Start New Jobs</h2>
+              <h4>Select an index</h4>
+            </div>
+            <div className="col-4">
+              <button className="btn btn-lg btn-info">Change Working Directory</button>
+            </div>
+          </div>
+          <div className="row">
+            <div className='col-12'>
+              <ChooseIndexButtons selectedIndex = {this.state.selectedIndex} onClickIndex={this.onClickIndex}/>
+              {this.state.changeIndexWarning ?
+                (<div class="alert alert-warning" role="alert">
+                  <strong>Warning!</strong> Start or cancel current job before starting a new one.
+                </div>) : ''}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              {this.state.selectedIndex ? (<div><h4>Choose {this.state.selectedIndex.toUpperCase()} Parameters</h4> {this.state.paramComp} </div>) : ''}
+            </div>
+          </div>
+          <div className="row">
+            <button className="btn btn-lg btn-info" onClick={this.cancelJob}>Cancel Job</button>
+          </div>
+        </div>
+    //   </div>
+    );
+  }
+}
+
+export default NewJobs;
