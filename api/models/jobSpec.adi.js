cd11a563004dc2624e211ab7aba810aa4d031884
Ot Gabaldon Torrents <gabaldonot@gmail.com>
2018-11-05 15:42:06 -0500

    Added JobSpec models and Discriminatories

---
+++ api/models/jobSpec.adi.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const JobSpec = require('./jobSpec');
+
+module.exports = JobSpec.discriminator('adi', new mongoose.Schema({
+  maxFreq:{type:Number,min:0,max:100000,default:16000},
+  dbThreshold:{type:Number,default:32},
+  freqStep:{type:Number,min:1,default:512},
+  shannon:{type:Boolean,deault:True},
+}));
