ca15906b699c72bf684541e5d161fe6589bc8781
Josh Pollmann <joshpollmann102@gmail.com>
2018-11-06 15:53:39 -0500

    Finish basic components, continue work on formatting

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 import Catalog from './components/catalog';
 
+const RESULTS = [
+  {name: 'UCF Arboretum', date: '10/19/2018', index: 'NDSI', params: 'preset 2'},
+  {name: 'Zoo', date: '11/5/2018', index: ['NDSI', 'ACI'], params: 'Default'}
+];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Catalog />
+        <Catalog
+          results={RESULTS} />
       </div>
     );
   }
 }
+
+
 
 export default App;
--- src/components/catalog.js
+++ src/components/catalog.js
@@ -1,68 +1,134 @@
 import React, { Component } from 'react';
 
-class Catalog extends Component {
+class WorkingDirectory extends Component {
+   render() {
+     const wd = this.props.wd;
+     return (
+       <div>
+         <button className="btn btn-lg btn-info">Change Working Directory</button>
+         <p>{wd}</p>
+       </div>
+     );
+   }
+}
 
-  constructor(props) {
-    super(props);
-
-    // Todo: add handlers here
-
-    // Todo: add state elements here
-
+class FilterJobs extends Component {
+  render() {
+    return(
+      <div class="search-container">
+        <form id="searchForm">
+          <input name="searchJob" type="text" placeholder="Search Jobs.." />
+          <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+        <div id="radioButtons">
+          <label class="radio-container">Completed
+            <input name="searchType" type="radio" value="Completed" />
+            <span class="checkmark"></span>
+          </label>
+          <label class="radio-container">In Progress
+            <input name="searchType" type="radio" value="In Progress" />
+            <span class="checkmark"></span>
+          </label>
+        </div>
+        <div id="filterIndex">
+          <label for="index"><b>Filter by index</b></label>
+          <select name="index">
+            <option value="ndsi">NDSI</option>
+            <option value="aci">ACI</option>
+            <option value="adi">ADI</option>
+            <option value="rms">RMS</option>
+          </select>
+        </div>
+      </div>
+    );
   }
+}
 
+class ResultTable extends Component {
+  render() {
+    const rows = [];
+    let lastName = null;
+
+    this.props.results.forEach((result) => {
+      if(result.name !== lastName) {
+        rows.push(
+          <ResultName
+            result={result.name}
+            key={result.name} />
+        );
+      }
+      rows.push(
+        <ResultRow
+          result={result}
+          key={result.name} />
+      );
+      lastName = result.name;
+    });
+
+    return(
+      <div class="table-wrapper">
+        <table>
+          <tbody>{rows}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+class ResultName extends Component {
+  render(){
+    const result = this.props.result;
+    return(
+      <tr>
+        <th>
+          <b>{result}</b>
+        </th>
+      </tr>
+    );
+  }
+}
+
+class ResultRow extends Component {
+  render() {
+    const result = this.props.result;
+    const resultDate = result.date;
+    const resultIndex = result.index;
+    const resultParam = result.params;
+    return(
+      <tr>
+        <td>{resultDate},</td>
+        <td>{resultIndex},</td>
+        <td>{resultParam}</td>
+      </tr>
+    );
+  }
+}
+
+class Catalog extends Component {
   render() {
     return (
       <div className="container-fluid">
-          <div className="row">
-            <div className="col">
-              <h3>Your Results Catalog</h3>
-            </div>
-            <div className="col">
-              <button className="btn btn-lg btn-info">Change Working Directory</button>
-              <p>Current working directory here</p>
-            </div>
+        <div className="row">
+          <div className="col">
+            <h3>Your Results Catalog</h3>
           </div>
-          <div className="row">
-            <div className="col-4">
-              <div className="row">
-                <div className="col">
-                  <div class="search-container">
-                    <form action="">
-                      <input name="searchJob" type="text" placeholder="Search Jobs.." />
-                      <button type="submit"><i class="fa fa-search"></i></button>
-                    </form>
-                  </div>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <label class="radio-container">Completed
-                    <input name="searchType" type="radio" value="Completed" />
-                    <span class="checkmark"></span>
-                  </label>
-                  <label class="radio-container">In Progress
-                    <input name="searchType" type="radio" value="In Progress" />
-                    <span class="checkmark"></span>
-                  </label>
-                </div>
-              </div>
-            </div>
-            <div className="col-8">
-              <div class="results-name-container">
-                <div className="row">
-                  <h5>Chosen Results name here</h5>
-                  <div className="col">
-                    <p>Chosen Results date/time here</p>
-                  </div>
-                  <div className="col">
-                    <p>Chosen Results parameters here</p>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="col">
+            <WorkingDirectory
+              wd="C:/WorkingDirectory" />
           </div>
         </div>
+        <div className="row">
+          <div className="col">
+            <FilterJobs />
+            <h4>Your Results</h4>
+            <ResultTable
+              results={this.props.results} />
+          </div>
+          <div className="col">
+            // ToDo: analysis component
+          </div>
+        </div>
+      </div>
     );
   }
 }
