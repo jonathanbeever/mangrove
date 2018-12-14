f3899af06d8ab1bfdfce1e42f95968455c4cd99b
David Palumbo <davejpalumbo@gmail.com>
2018-12-14 12:13:11 -0500

    Changed big number to a power of 2 for readability

--- server/test/util/mockObjectId.js
+++ server/test/util/mockObjectId.js
@@ -3,7 +3,7 @@ const mockObjectId = (n) => {
     return `Error: Cannot create ObjectId from non-integer (${n}).`;
   } if (n < 0) {
     return `Error: Cannot create ObjectId from negative number (${n}).`;
-  } if (n > 79228162514264337593543950336) {
+  } if (n > 2 ** 96) {
     return `Error: Number too large to create ObjectId (${n}).`;
   }
 
