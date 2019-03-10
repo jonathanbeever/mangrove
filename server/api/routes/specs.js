9e926640254305bb9fa152b18a845aefe0534ccb
David Palumbo <davejpalumbo@gmail.com>
2019-03-09 21:46:16 -0500

    Simplified API routes (Promise to async/await)

--- server/api/routes/specs.js
+++ server/api/routes/specs.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const mongoose = require('mongoose');
+const config = require('config');
 
 const { Spec } = require('../models/spec');
 const {
@@ -17,8 +18,10 @@ const Type = require('../models/type');
 
 const { arrayDiff } = require('../../util/array');
 
+const error = config.get('error');
+
 // Create Spec
-router.put('/', (req, res) => {
+router.put('/', async (req, res) => {
   const missingKeys = arrayDiff(newSpecKeys(), Object.keys(req.body));
   if (missingKeys.length > 0) {
     return res.status(400).json({
@@ -46,115 +49,101 @@ router.put('/', (req, res) => {
   try {
     validateParams(req.body.type, params);
   } catch (err) {
-    return res.status(400).json({
-      message: err.message,
-    });
+    return res.status(400).json({ message: err.message });
   }
+  const paramsFilled = fillDefaultParams(req.body.type, params);
 
-  SpecModel.find(fillDefaultParams(req.body.type, params))
-    .exec()
-    .then((searchResult) => {
-      if (searchResult.length /* === 1 */) {
-        res.status(200).json({
-          specId: searchResult[0]._id,
-          type: specTypeToType(searchResult[0].type),
-          ...getParamsFromSpec(searchResult[0]),
-        });
-      } else {
-        const spec = new SpecModel({
-          _id: new mongoose.Types.ObjectId(),
-          type: specType,
-          ...params,
-        });
+  try {
+    const searchResult = await SpecModel.find(paramsFilled).exec();
 
-        Spec.create(spec)
-          .then((createResult) => {
-            res.status(201).json({
-              specId: createResult._id,
-              type: specTypeToType(createResult.type),
-              ...getParamsFromSpec(createResult),
-            });
-          })
-          .catch((err) => {
-            res.status(500).json({
-              error: err,
-            });
-          });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
+    if (searchResult.length /* === 1 */) {
+      return res.status(200).json({
+        specId: searchResult[0]._id,
+        type: specTypeToType(searchResult[0].type),
+        ...getParamsFromSpec(searchResult[0]),
       });
+    }
+
+    const spec = new SpecModel({
+      _id: new mongoose.Types.ObjectId(),
+      type: specType,
+      ...params,
     });
+
+    const createResult = await Spec.create(spec);
+
+    return res.status(201).json({
+      specId: createResult._id,
+      type: specTypeToType(createResult.type),
+      ...getParamsFromSpec(createResult),
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: error.internal });
+  }
 });
 
 // Get Spec
-router.get('/:specId', (req, res) => {
+router.get('/:specId', async (req, res) => {
   const { specId } = req.params;
 
-  Spec.findById(specId)
-    .exec()
-    .then((searchResult) => {
-      if (searchResult) {
-        res.status(200).json({
-          specId: searchResult._id,
-          type: specTypeToType(searchResult.type),
-          ...getParamsFromSpec(searchResult),
-        });
-      } else {
-        res.status(404).json({
-          message: `No valid entry found for ${specId}`,
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
+  try {
+    const searchResult = await Spec.findById(specId).exec();
+
+    if (!searchResult) {
+      return res.status(404).json({
+        message: `No valid entry found for ${specId}`,
       });
+    }
+
+    return res.status(200).json({
+      specId: searchResult._id,
+      type: specTypeToType(searchResult.type),
+      ...getParamsFromSpec(searchResult),
     });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: error.internal });
+  }
 });
 
 // Get All Specs
-router.get('/', (req, res) => {
-  Spec.find()
-    .exec()
-    .then((searchResult) => {
-      res.status(200).json({
-        count: searchResult.length,
-        specs: searchResult.map(spec => ({
-          specId: spec._id,
-          type: specTypeToType(spec.type),
-          ...getParamsFromSpec(spec),
-        })),
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-      });
+router.get('/', async (req, res) => {
+  try {
+    const searchResult = await Spec.find().exec();
+
+    return res.status(200).json({
+      count: searchResult.length,
+      specs: searchResult.map(spec => ({
+        specId: spec._id,
+        type: specTypeToType(spec.type),
+        ...getParamsFromSpec(spec),
+      })),
     });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: error.internal });
+  }
 });
 
 // Delete Spec
-router.delete('/:specId', (req, res) => {
+router.delete('/:specId', async (req, res) => {
   const { specId } = req.params;
-  Spec.deleteOne({ _id: specId })
-    .exec()
-    .then((deleteResult) => {
-      res.status(200).json({
-        success: true,
-        message:
-          deleteResult.n > 0
-            ? `Succesfully deleted Spec with specId: ${specId}`
-            : `No valid entry found for specId: ${specId}.`,
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-      });
+
+  try {
+    const deleteResult = await Spec.deleteOne({ _id: specId }).exec();
+
+    return res.status(200).json({
+      success: true,
+      message:
+      deleteResult.n > 0
+        ? `Succesfully deleted Spec with specId: ${specId}`
+        : `No valid entry found for specId: ${specId}.`,
     });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: error.internal });
+  }
 });
 
 module.exports = router;
