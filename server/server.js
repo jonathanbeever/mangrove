4470ac3caff25275ad7d66e3f61099d10d08d149
David Palumbo <davejpalumbo@gmail.com>
2019-04-02 22:14:32 -0400

    Graceful server shutdown upon exit/restart

--- server/server.js
+++ server/server.js
@@ -1,26 +1,52 @@
 const http = require('http');
+
 const config = require('config');
 
-const conn = require('./util/db');
+const dbConnection = require('./util/db');
 const app = require('./app');
-
-const port = config.get('port') || 3000;
-const server = http.createServer(app);
-
 const jobQueue = require('./util/jobQueue');
 
-conn
-  .open()
-  .then(async () => {
-    try {
-      global.jobQueue = await jobQueue.init();
-      server.listen(port, () => {
-        // OK
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const port = config.get('port');
+const server = http.createServer(app);
+
+global.jobQueue = jobQueue;
+
+async function gracefulShutdown() {
+  process.stdout.write('Closing HTTP server...');
+  await server.close();
+  process.stdout.write('  \tOK\n');
+  process.stdout.write('Closing job queue...');
+  await global.jobQueue.destroy();
+  process.stdout.write('\t\tOK\n');
+  process.stdout.write('Closing database connection...');
+  await dbConnection.close();
+  process.stdout.write('\tOK\n');
+  process.stdout.write('Successfully exited Mangrove server\n');
+}
+
+process.on('SIGINT', async () => {
+  await gracefulShutdown();
+  process.exitCode = 0;
+});
+process.on('SIGTERM', async () => {
+  await gracefulShutdown();
+  process.exitCode = 0;
+});
+process.once('SIGUSR2', async () => {
+  await gracefulShutdown();
+  process.kill(process.pid, 'SIGUSR2');
+});
+
+(async () => {
+  try {
+    process.stdout.write('Starting Mangrove server...');
+    await dbConnection.open();
+    await global.jobQueue.init();
+
+    server.listen(port, () => process.stdout.write(' Ready\n'));
+  } catch (err) {
+    console.error(err);
+    gracefulShutdown();
+    process.exitCode = 1;
+  }
+})();
