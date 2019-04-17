2e844d87822d16f9eecb6257a53cbc7acc71a2dc
keithguske <keith@guske.net>
2019-04-17 09:35:30 -0400

    Removes `request` dependency from client

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Login from './components/login';
 
 const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
-const request = require('request');
 const jwkToPem = require('jwk-to-pem');
 const jwt = require('jsonwebtoken');
 
