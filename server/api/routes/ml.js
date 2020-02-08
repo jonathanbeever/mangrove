7173194c1ef169dd7c37c29483068f1c3644c889
James <jamesgupchurch@knights.ucf.edu>
2020-02-08 00:15:55 -0500

    removed unnecessary imports for now

--- server/api/routes/ml.js
+++ server/api/routes/ml.js
@@ -1,16 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-const mongoose = require('mongoose');
-const moment = require('moment');
-const config = require('config');
 
 const multer = require('multer');
 
-const logger = require('../../util/logger');
-
-const { arrayDiff } = require('../../util/array');
-const { verify, getUser } = require('../../util/verify');
 const { generateSpectrograms } = require('../../util/audio_manipulation');
 
 // TODO: Fix upload destination
