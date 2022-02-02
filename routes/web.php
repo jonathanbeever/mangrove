e02d3ee43b5d40e906db5214a2d2a7e6d1773f5f
BunnyCorruption <huntercharriman@gmail.com>
2022-02-02 00:35:00 -0500

    - Changed homepage to login - Added a redirect to register page from login page.

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
+    return Inertia::render('Auth/Login', [
+        //'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
