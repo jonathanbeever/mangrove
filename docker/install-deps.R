c43ab5f90c179d50a617592fa87e8b84c9a89537
aidanthewiz <aidanartherton@gmail.com>
2022-02-05 04:09:06 -0500

    Implement development docker Rscript and ffmpeg

---
+++ docker/install-deps.R
@@ -0,0 +1,2 @@
+install.packages(c("tuneR", "devtools"), dependencies=TRUE, repos="https://cloud.r-project.org")
+devtools::install_github("OtGabaldon/soundecology", ref="a78393ab9929955a0151e5766a08bfbdef168ecd", subdir="soundecology")
