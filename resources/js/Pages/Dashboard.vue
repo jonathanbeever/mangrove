a9c3b166dc2848efad603a754080f338a4cda994
Seth.Brockett <sethrbrockett@outlook.com>
2022-03-25 21:07:12 -0700

    dynamic id's for charts and slight fix to error checking

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -9,7 +9,7 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <test :xBarLabels="[1, 2, 3, 4]" :dataSetLabels="['label1', 'label2']" :dataSetData="[[1, 2, 3, 4], [2, 4, 5, 6]]" :xLabel="'xLabel'" :yLabel="'yLabel'"/>
+                    <test :id="'chartid'" :xBarLabels="[1, 2, 3, 4]" :dataSetLabels="['label1', 'label2']" :dataSetData="[[1, 2, 3, 4], [2, 4, 5, 6]]" :xLabel="'xLabel'" :yLabel="'yLabel'"/>
                 </div>
             </div>
         </div>
