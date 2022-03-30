71b781cb9dd083541e01e5b569fa4e176d68f3df
Aidanthewiz <aidanartherton@gmail.com>
2022-03-29 20:45:03 -0400

    Fix queue current route mapping

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -24,7 +24,7 @@
                                 <jet-nav-link :href="route('jobs.create')" :active="route().current('jobs.create')">
                                     Create Jobs
                                 </jet-nav-link>
-                                <jet-nav-link :href="route('jobs.index')" :active="route().current('queue')">
+                                <jet-nav-link :href="route('jobs.index')" :active="route().current('jobs.index')">
                                     Job Queue
                                 </jet-nav-link>
                                 <jet-nav-link :href="route('about')" :active="route().current('about')">
@@ -153,7 +153,7 @@
                         <jet-responsive-nav-link :href="route('jobs.create')" :active="route().current('jobs.create')">
                             Create Jobs
                         </jet-responsive-nav-link>
-                        <jet-responsive-nav-link :href="route('jobs.index')" :active="route().current('queue')">
+                        <jet-responsive-nav-link :href="route('jobs.index')" :active="route().current('jobs.index')">
                             Job Queue
                         </jet-responsive-nav-link>
                         <jet-responsive-nav-link :href="route('about')" :active="route().current('about')">
