2cf03b2d079bf75eed7d942fefccbc6e9ca1e180
Aidanthewiz <aidanartherton@gmail.com>
2022-04-14 18:54:55 -0400

    Add zoom support to line charts

--- resources/js/Pages/ChartVisualizations/DualLine.vue
+++ resources/js/Pages/ChartVisualizations/DualLine.vue
@@ -5,117 +5,127 @@
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
-    import JetApplicationLogo from '@/Jetstream/ApplicationLogo.vue'
-    import JetButton from '@/Jetstream/Button.vue'
-    import JetLabel from '@/Jetstream/Label.vue'
-    import JetInput from '@/Jetstream/Input.vue'
-    import { Chart, Legend, BarController, LineController, Title, CategoryScale, LinearScale, PointElement, LineElement, BarElement, Tooltip } from 'chart.js'
+import {defineComponent} from 'vue'
+import JetApplicationLogo from '@/Jetstream/ApplicationLogo.vue'
+import JetButton from '@/Jetstream/Button.vue'
+import JetLabel from '@/Jetstream/Label.vue'
+import JetInput from '@/Jetstream/Input.vue'
+import {Chart, Legend, BarController, LineController, Title, CategoryScale, LinearScale, PointElement, LineElement, BarElement, Tooltip} from 'chart.js'
+import zoomPlugin from 'chartjs-plugin-zoom';
 
-    const makeRange = (start, end) => {
-        return Array(end - start + 1).fill().map((_, idx) => start + idx)
-    }
+const makeRange = (start, end) => {
+    return Array(end - start + 1).fill().map((_, idx) => start + idx)
+}
 
-    export default defineComponent({
-        components: {
-            JetApplicationLogo,
-            JetButton,
-            JetLabel,
-            JetInput
-        },
-        props: ['xBarLabels', 'dataSetLabels', 'dataSetData', 'yLabel', 'xLabel', 'title', 'id'],
-        data: function () {
-            return {
-            }
-        },
-        mounted: function () {
-            Chart.register(LineController, Title, Legend, BarController, CategoryScale, LinearScale, PointElement, LineElement, BarElement)
-            var ctx = document.getElementById(this.id).getContext('2d')
-            var dataFirst = {
-                label: this.dataSetLabels[0],
-                data: this.dataSetData[0],
-                lineTension: 1,
-                fill: false,
-                borderColor: 'green'
-            };
+export default defineComponent({
+    props: ['xBarLabels', 'dataSetLabels', 'dataSetData', 'yLabel', 'xLabel', 'title', 'id'],
+    components: {
+        JetApplicationLogo,
+        JetButton,
+        JetLabel,
+        JetInput
+    },
+    data: function () {
+        return {}
+    },
+    mounted: function () {
+        Chart.register(LineController, Title, Legend, BarController, CategoryScale, LinearScale, PointElement, LineElement, BarElement, zoomPlugin)
+        var ctx = document.getElementById(this.id).getContext('2d')
+        var dataFirst = {
+            label: this.dataSetLabels[0],
+            data: this.dataSetData[0],
+            lineTension: 1,
+            fill: false,
+            borderColor: 'green'
+        };
 
-            var dataSecond = {
-                label: this.dataSetLabels[1],
-                data: this.dataSetData[1],
-                lineTension: 1,
-                fill: false,
-                borderColor: 'red'
-            };
+        var dataSecond = {
+            label: this.dataSetLabels[1],
+            data: this.dataSetData[1],
+            lineTension: 1,
+            fill: false,
+            borderColor: 'red'
+        };
 
-            var chartData = {
-                labels: this.xBarLabels,
-                datasets: [
-                    dataFirst,
-                    dataSecond
-                ]
-            }
+        var chartData = {
+            labels: this.xBarLabels,
+            datasets: [
+                dataFirst,
+                dataSecond
+            ]
+        }
 
-            var chartOptions = {
-                plugins: {
-                    tooltips:{
+        var chartOptions = {
+            plugins: {
+                tooltips: {
                     mode: 'index'
                 },
                 hover: {
                     mode: 'index'
                 },
-                    legend: {
-                        display: true,
-                        responsive: true,
-                        position: 'top',
-                        labels: {
-                            boxWidth: 80,
-                            font: {
-                                color: 'black',
-                                size: 20
-                            }
+                legend: {
+                    display: true,
+                    responsive: true,
+                    position: 'top',
+                    labels: {
+                        boxWidth: 80,
+                        font: {
+                            color: 'black',
+                            size: 20
                         }
-                    },
-                    title: {
-                            display: true,
-                            text: this.title,
-                            font: {
-                                color: 'black',
-                                size: 20
-                            }
                     }
                 },
-                scales: {
-                    x: {
-                        title: {
-                            display: true,
-                            text: this.xLabel,
-                            font: {
-                                color: 'black',
-                                size: 20
-                            }
+                title: {
+                    display: true,
+                    text: this.title,
+                    font: {
+                        color: 'black',
+                        size: 20
+                    }
+                },
+                zoom: {
+                    zoom: {
+                        wheel: {
+                            enabled: true,
+                        },
+                        pinch: {
+                            enabled: true
+                        },
+                        mode: 'x',
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: this.xLabel,
+                        font: {
+                            color: 'black',
+                            size: 20
                         }
-                    },
-                    y: {
-                        title: {
-                            display: true,
-                            text: this.yLabel,
-                            font: {
-                                color: 'black',
-                                size: 20
-                            }
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: this.yLabel,
+                        font: {
+                            color: 'black',
+                            size: 20
                         }
                     }
                 }
-            };
+            }
+        };
 
-            var chart = new Chart(ctx,
+        var chart = new Chart(ctx,
             {
                 type: 'line',
                 data: chartData,
                 options: chartOptions
             })
-        },
-        methods: {
-        }
-    } )
+    },
+    methods: {}
+})
 </script>
--- resources/js/Pages/ChartVisualizations/SingleLine.vue
+++ resources/js/Pages/ChartVisualizations/SingleLine.vue
@@ -5,106 +5,117 @@
 </template>
 
 <script>
-    import { defineComponent } from 'vue'
-    import JetApplicationLogo from '@/Jetstream/ApplicationLogo.vue'
-    import JetButton from '@/Jetstream/Button.vue'
-    import JetLabel from '@/Jetstream/Label.vue'
-    import JetInput from '@/Jetstream/Input.vue'
-    import { Chart, Legend, BarController, LineController, Title, CategoryScale, LinearScale, PointElement, LineElement, BarElement, Tooltip } from 'chart.js'
+import {defineComponent} from 'vue'
+import JetApplicationLogo from '@/Jetstream/ApplicationLogo.vue'
+import JetButton from '@/Jetstream/Button.vue'
+import JetLabel from '@/Jetstream/Label.vue'
+import JetInput from '@/Jetstream/Input.vue'
+import {Chart, Legend, BarController, LineController, Title, CategoryScale, LinearScale, PointElement, LineElement, BarElement, Tooltip} from 'chart.js'
+import zoomPlugin from 'chartjs-plugin-zoom';
 
-    const makeRange = (start, end) => {
-        return Array(end - start + 1).fill().map((_, idx) => start + idx)
-    }
+const makeRange = (start, end) => {
+    return Array(end - start + 1).fill().map((_, idx) => start + idx)
+}
 
-    export default defineComponent({
-        components: {
-            JetApplicationLogo,
-            JetButton,
-            JetLabel,
-            JetInput
-        },
-        props: ['xBarLabels', 'dataSetLabels', 'dataSetData', 'yLabel', 'xLabel', 'title', 'id'],
-        data: function () {
-            return {
-            }
-        },
-        mounted: function () {
-            Chart.register(LineController, Title, Legend, BarController, CategoryScale, LinearScale, PointElement, LineElement, BarElement)
-            var ctx = document.getElementById(this.id).getContext('2d')
-            var dataFirst = {
-                label: this.dataSetLabels[0],
-                data: this.dataSetData[0],
-                lineTension: 1,
-                fill: false,
-                borderColor: 'green'
-            };
+export default defineComponent({
+    components: {
+        JetApplicationLogo,
+        JetButton,
+        JetLabel,
+        JetInput
+    },
+    props: ['xBarLabels', 'dataSetLabels', 'dataSetData', 'yLabel', 'xLabel', 'title', 'id'],
+    data: function () {
+        return {}
+    },
+    mounted: function () {
+        Chart.register(LineController, Title, Legend, BarController, CategoryScale, LinearScale, PointElement, LineElement, BarElement, zoomPlugin)
+        var ctx = document.getElementById(this.id).getContext('2d')
+        var dataFirst = {
+            label: this.dataSetLabels[0],
+            data: this.dataSetData[0],
+            lineTension: 1,
+            fill: false,
+            borderColor: 'green'
+        };
 
-            var chartData = {
-                labels: this.xBarLabels,
-                datasets: [
-                    dataFirst
-                ]
-            }
+        var chartData = {
+            labels: this.xBarLabels,
+            datasets: [
+                dataFirst
+            ]
+        }
 
-            var chartOptions = {
-                tooltips:{
-                    mode: 'index'
-                },
-                hover: {
-                    mode: 'index'
-                },
-                plugins: {
-                    legend: {
-                        display: true,
-                        responsive: true,
-                        position: 'top',
-                        labels: {
-                            boxWidth: 80,
-                            font: {
-                                color: 'black',
-                                size: 20
-                            }
+        var chartOptions = {
+            tooltips: {
+                mode: 'index'
+            },
+            hover: {
+                mode: 'index'
+            },
+            plugins: {
+                legend: {
+                    display: true,
+                    responsive: true,
+                    position: 'top',
+                    labels: {
+                        boxWidth: 80,
+                        font: {
+                            color: 'black',
+                            size: 20
                         }
-                    },
-                    title: {
-                            display: true,
-                            text: this.title,
-                            font: {
-                                color: 'black',
-                                size: 20
-                            }
                     }
                 },
-                scales: {
-                    x: {
-                        title: {
-                            display: true,
-                            text: this.xLabel,
-                            font: {
-                                color: 'black',
-                                size: 20
-                            }
+                title: {
+                    display: true,
+                    text: this.title,
+                    font: {
+                        color: 'black',
+                        size: 20
+                    }
+                },
+                zoom: {
+                    zoom: {
+                        wheel: {
+                            enabled: true,
+                        },
+                        pinch: {
+                            enabled: true
+                        },
+                        mode: 'x',
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: this.xLabel,
+                        font: {
+                            color: 'black',
+                            size: 20
                         }
-                    },
-                    y: {
-                        title: {
-                            display: true,
-                            text: this.yLabel,
-                            font: {
-                                color: 'black',
-                                size: 20
-                            }
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: this.yLabel,
+                        font: {
+                            color: 'black',
+                            size: 20
                         }
                     }
                 }
-            };
+            }
+        };
 
-            var chart = new Chart(ctx,
+        var chart = new Chart(ctx,
             {
                 type: 'line',
                 data: chartData,
                 options: chartOptions
             })
-        }
-    } )
+    }
+})
 </script>
