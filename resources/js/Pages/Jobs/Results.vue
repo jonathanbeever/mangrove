f6365c1cc739f641f6c8508840f4e9ce730b57d1
cg021 <christiang021@gmail.com>
2022-04-16 18:38:25 -0400

    update region

--- resources/js/Pages/Jobs/Results.vue
+++ resources/js/Pages/Jobs/Results.vue
@@ -371,14 +371,7 @@ export default defineComponent({
                             end: 3,
                             loop: false,
                             color: 'hsla(400, 100%, 30%, 0.5)'
-                        }, {
-                            start: 5,
-                            end: 7,
-                            loop: false,
-                            color: 'hsla(200, 50%, 70%, 0.4)',
-                            minLength: 1,
-                            drag:true
-                        }
+                        },
                     ],
                     dragSelection: {
                         slop: 5
