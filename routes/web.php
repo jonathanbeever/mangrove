24a88a30fd27484040c04ace297d5ebe432cc429
Aidanthewiz <aidanartherton@gmail.com>
2022-04-17 00:39:30 -0400

    Name import routes

--- routes/web.php
+++ routes/web.php
@@ -32,8 +32,8 @@ Route::middleware([
     'verified',
 ])->group(static function () {
     Route::resource('jobs', JobController::class)->except(['show', 'edit', 'update']);
-    Route::get('/import', [ImportController::class, 'create']);
-    Route::post('/import', [ImportController::class, 'import']);
+    Route::get('/import', [ImportController::class, 'create'])->name('import.create');
+    Route::post('/import', [ImportController::class, 'import'])->name('import.save');
 
     Route::get('/about', static function () {
         return Inertia::render('About');
