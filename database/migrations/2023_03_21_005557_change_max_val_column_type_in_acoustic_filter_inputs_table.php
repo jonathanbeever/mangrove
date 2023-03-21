6e8237d9ee849624c18aea64cb1889b034b7b910
Noah Trim <55414467+Trimendously@users.noreply.github.com>
2023-03-20 21:45:47 -0400

    Changing max_val to decimal
    Changing the max_val in acoustic filter table schema from integer to decimal

---
+++ database/migrations/2023_03_21_005557_change_max_val_column_type_in_acoustic_filter_inputs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('acoustic_filter_inputs', function (Blueprint $table) {
+            $table->decimal('max_val', 10, 2)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('acoustic_filter_inputs', function (Blueprint $table) {
+            //
+        });
+    }
+};
