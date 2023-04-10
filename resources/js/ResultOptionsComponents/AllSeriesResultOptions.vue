4cc3035d92b557cf3415b66097292a2dc5434bf2
Christopher Gray <gray.chris@Knights.ucf.edu>
2023-04-10 00:48:25 +0000

    Formatting

--- resources/js/ResultOptionsComponents/AllSeriesResultOptions.vue
+++ resources/js/ResultOptionsComponents/AllSeriesResultOptions.vue
@@ -42,7 +42,6 @@ export default defineComponent({
                     options: this.indexOptions,
                     labelKey: null,
                 },
-
             }
         },
         indexOptions() {
