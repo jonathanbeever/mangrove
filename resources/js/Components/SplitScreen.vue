3a94bd824c7f843122c45832729475756db6b7da
Christopher Gray <gray.chris@Knights.ucf.edu>
2023-04-11 18:16:27 +0000

    Adds SplitScreen component base

---
+++ resources/js/Components/SplitScreen.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="flex flex-row">
+    <slot name="left" >
+
+    </slot>
+    <!-- Draggable thing -->
+    <slot name="right" >
+
+    </slot>
+</div>
+</template>
+
+<script>
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    data() {
+        return {
+
+        }
+    }
+    // onDrag watcher?
+})
+</script>
